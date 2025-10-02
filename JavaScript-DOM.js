ba43cf6e132be327f265c484b30c65e23778be19
Abdur Rahman <abdurrahman21266057@gmail.com>
2025-10-02 10:21:43 +0600

    JavaScript-DOM or css Changing

--- JavaScript-DOM.js
+++ JavaScript-DOM.js
@@ -1,18 +1,30 @@
-function foramCerat (){
-const Fform = document.forms['myForm']
+// function foramCerat (){
+// const Fform = document.forms['myForm']
+
+// const value = Fform['fname'].value;
+
+// if(value === ""){
+
+//     alert("form is not valide")
+// }
+
+// console.log(value);
+
+// return false;
+// }
+
+setTimeout(function () {
+  const cssStyle = document.querySelector("#dome");
+  cssStyle.style.color = "red";
+  cssStyle.style.fontSize = "50px";
+},3000);
 
 
-const value = Fform['fname'].value;
 
 
-if(value === ""){
+  function button () {
+  const cssStyle = document.querySelector("#dome");
 
-
-    alert("form is not valide")
-}
-
-
-console.log(value);
-
-return false;
+  cssStyle.style.color = "";
+  cssStyle.style.fontSize = "";
 }
