2dd195dee001f60a3bb53410ed399cfd93f0bc9a
Abdur Rahman <abdurrahman21266057@gmail.com>
2025-10-05 13:04:33 +0600

    addEventListener

---
+++ addEventListeners.js
@@ -0,0 +1,22 @@
+function myFunction (){
+
+
+    console.log( "click ");
+    
+}
+
+const button1 = document.getElementById('button1')
+
+
+
+button1.addEventListener('click', myFunction);
+button1.addEventListener('mouseover', function(){
+
+
+ console.log('jlaksddfg');
+ 
+    
+});
+                        //   Syntax
+
+// element.addEventListener(event, function, useCapture);
